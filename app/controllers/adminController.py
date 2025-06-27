ff034c400c5d27cf33f5ca17d48b5960b1279205
sharath <sharathshetty301@gmail.com>
2025-06-27 18:15:01 +0530

    admin can add product

--- app/controllers/adminController.py
+++ app/controllers/adminController.py
@@ -3,7 +3,8 @@
 from fastapi import APIRouter, Request, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
-from app.models.schema import User
+from app.models.schema import User ,Product
+from fastapi.responses import  RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="app/templates")
@@ -23,3 +24,19 @@ def admin_dashboard(request: Request, db: Session = Depends(get_db)):
         "request": request,
         "users": users
     })
+
+@router.get("/products/add")
+def show_add_product_form(request: Request):
+    return templates.TemplateResponse("admin_add_product.html", {"request": request})
+
+
+@router.post("/products/add")
+async def add_product(request: Request, db: Session = Depends(get_db)):
+    form = await request.form()
+    name = form.get("name")
+    price = float(form.get("price"))
+
+    new_product = Product(name=name, price=price)
+    db.add(new_product)
+    db.commit()
+    return RedirectResponse(url="/", status_code=302)
